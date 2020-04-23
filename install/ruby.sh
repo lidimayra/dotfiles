64b3c82e7268a3b1a0089e9dd2c47bb777bc8151
Lidiane Taquehara <lidi.mayra@gmail.com>
2020-04-22 22:38:08 -0300

    Replace RVM with chruby
    - Remove RVM
    - Use chruby to manage ruby versions:
        https://github.com/postmodern/chruby

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-# Install mpapis public key
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# Download and install chruby
+cd ~
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9
+sudo make install
+sudo ./scripts/setup.sh
+rm ~/chruby-0.3.9.tar.gz
 
-# Install RVM stable with rails
-\curl -sSL https://get.rvm.io | bash -s stable --rails
+# Download and install ruby-install
+cd ~
+wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
+tar -xzvf ruby-install-0.7.0.tar.gz
+cd ruby-install-0.7.0/
+sudo make install
+rm ~/ruby-install-0.7.0.tar.gz
